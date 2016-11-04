a34373d9bf96d6d57f4b05ea6c5b4dbca9729e59
Michal Wladyslaw Sieczkowski <msieczko@lab011.elka.pw.edu.pl>
2016-11-04 11:44:06 +0100

    Initial commit

---
+++ Release/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mion/s/143/msieczko/Desktop/Liniowe/src
+# Build directory: /home/mion/s/143/msieczko/Desktop/Liniowe/Release/src
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
