631319e8da6601ca6a94eceb05d3a16ea537909b
Michał Sieczkowski <mikee2509@gmail.com>
2016-12-09 10:47:14 +0100

    Delete Release and Debug directories, add .gitignore

--- Release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mion/s/143/msieczko/Desktop/Liniowe
-# Build directory: /home/mion/s/143/msieczko/Desktop/Liniowe/Release
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(tests)
--- Release/tests/CMakeFiles/aisdiLinearTests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for aisdiLinearTests.
-# This may be replaced when dependencies are built.
